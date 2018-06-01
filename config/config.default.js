54f89998ae7ee3de0ce9b8d36ef6018c11a80c14
孙亮(平安好房技术中心企业产品研发团队网站前端应用研发组) <SUNLIANG033@paicdom.local>
2018-06-01 14:25:07 +0800

    init

---
+++ config/config.default.js
@@ -0,0 +1,13 @@
+exports.keys = 'mykeys'
+
+exports.view = {
+  defaultViewEngine: 'nunjucks',
+  mapping: {
+    '.tpl': 'nunjucks',
+  },
+};
+
+exports.news = {
+  pageSize: 5,
+  serverUrl: 'https://hacker-news.firebaseio.com/v0',
+};
