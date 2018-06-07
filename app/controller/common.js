05d9cfa44a36e64ab0deb35532a24ee07188b3e0
孙亮(平安好房技术中心企业产品研发团队网站前端应用研发组) <SUNLIANG033@paicdom.local>
2018-06-07 19:29:46 +0800

    awaitWriteStream

--- app/controller/common.js
+++ app/controller/common.js
@@ -1,36 +1,39 @@
 'use strict';
 
 const Controller = require('egg').Controller;
-const sendToWormhole = require('stream-wormhole');
 const path = require('path')
 const fs = require('fs')
-const md5 = require('md5')
+const crypto = require('crypto')
+const sendToWormhole = require('stream-wormhole')
+// wait a write stream
+const awaitWriteStream = require('await-stream-ready').write
 
 class CommonController extends Controller {
   async uploadfile() {
     const ctx = this.ctx;
     const fileStream = await ctx.getFileStream();
-
+    // 目标路径
     const path = this.config.path.baseDir + '/app/public/' + fileStream.fieldname
-    
-    if (!fs.existsSync(path)) {
-        fs.mkdirSync(path);
-    }
-
+    // 文件夹不存在  则创建文件夹
+    if (!fs.existsSync(path)) fs.mkdirSync(path);
+    // 获取文件后缀
     let p = fileStream.filename.lastIndexOf('.') == -1 ? '' : fileStream.filename.lastIndexOf('.')
     let t = fileStream.filename.substr(Number(p))
-    let n = md5(fileStream.filename + new Date().toString()) + t.toString()
+    // 文件名+时间进行hash
+    const hash = crypto.createHash('md5')
+    hash.update(fileStream.filename + new Date().toString())
+    // hash后的文件名
+    let n = hash.digest('hex') + t.toString()
 
-    fileStream.on('data', chunk => {
-        console.log(`Received ${chunk.length} bytes of data  `, `Name: ${fileStream.filename}`)
-        fs.writeFile(path + '/' + n, chunk, err => {
-            if(err) throw err
-        })
-    })
+    const writeStream = fs.createWriteStream(path + '/' + n);
 
-    fileStream.on('end', () => {
-        console.log('write file end')
-    })
+    try {
+        await awaitWriteStream(fileStream.pipe(writeStream))
+    } catch (error) {
+        await sendToWormhole(stream);
+        throw err;
+    }
+
     const url = `${this.config.path.baseUrl}/public/${fileStream.fieldname}/${n}`
     ctx.body = ctx.__success(url)
   }
